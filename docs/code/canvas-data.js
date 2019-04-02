15642c3bec38e500db79a9120ec29bb5ac3c3c67
外部运力部-前端-方志青 <409951398@qq.com>
2019-04-02 15:54:57 +0800

    update

---
+++ docs/code/canvas-data.js
@@ -0,0 +1,100 @@
+const displayRow = [{
+  "index": 0,
+  "y": 32
+},
+{
+  "index": 1,
+  "y": 60
+},
+{
+  "index": 2,
+  "y": 88
+},
+{
+  "index": 3,
+  "y": 116
+},
+{
+  "index": 4,
+  "y": 144
+},
+{
+  "index": 5,
+  "y": 172
+},
+{
+  "index": 6,
+  "y": 200
+},
+{
+  "index": 7,
+  "y": 228
+},
+{
+  "index": 8,
+  "y": 256
+}, {
+  "index": 9,
+  "y": 284
+}, {
+  "index": 10,
+  "y": 312
+}, {
+  "index": 11,
+  "y": 340
+}, {
+  "index": 12,
+  "y": 368
+}, {
+  "index": 13,
+  "y": 396
+}, {
+  "index": 14,
+  "y": 424
+}, {
+  "index": 15,
+  "y": 452
+}, {
+  "index": 16,
+  "y": 480
+}, {
+  "index": 17,
+  "y": 508
+}, {
+  "index": 18,
+  "y": 536
+}, {
+  "index": 19,
+  "y": 564
+}, {
+  "index": 20,
+  "y": 592
+}, {
+  "index": 21,
+  "y": 620
+}, {
+  "index": 22,
+  "y": 648
+}
+]
+
+const cols = [{
+  "key": "$$index",
+  "label": "序号",
+  "width": 40,
+  "fixed": "left",
+  "show": true,
+  "_index": 0,
+  "x": 0
+}, {
+  "key": "$$operation",
+  "label": "操作",
+  "width": 80,
+  "fixed": "left",
+  "show": true,
+  "_index": 1,
+  "x": 40
+}]
+export default {
+  displayRow, cols
+}
